f2a5ac4bbbdf6670dfeeccaae392ff396743a1c4
“BHOOMIKANAIKgit config --global user.email “18004bhoomikanaik@gmail.com <18004bhoomikanaikgmail.com>
2026-05-27 12:54:19 +0530

    added docker file

--- src/main/java/cpm/jsp/empapi/security/SecurityConfig.java
+++ src/main/java/cpm/jsp/empapi/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package cpm.jsp.empapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,9 +14,12 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
+
             .authorizeHttpRequests(auth -> auth
-                    .anyRequest().authenticated()
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").permitAll()
+                .anyRequest().authenticated()
             )
+
             .httpBasic(httpBasic -> {});
 
         return http.build();
